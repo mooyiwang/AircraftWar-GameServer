8128f4535f2b0ede77ffd4d23a08572a16c419be
muyiWang <Whywmy@outlook.com>
2022-06-14 21:46:55 +0800

    完成对战交换

--- app/lib/src/main/java/com/hit/sz/lib/server/execute/PlayerMatch.java
+++ app/lib/src/main/java/com/hit/sz/lib/server/execute/PlayerMatch.java
@@ -3,7 +3,6 @@ package com.hit.sz.lib.server.execute;
 import com.hit.sz.lib.IOStream.MyObjectInputStream;
 import com.hit.sz.lib.data.CheckData;
 import com.hit.sz.lib.data.DataPackage;
-import com.hit.sz.lib.data.UserData;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -14,27 +13,43 @@ public class PlayerMatch extends Thread{
     private DataPackage dataPackage;
     private MyObjectInputStream objIn;
     private ObjectOutputStream objOut;
-    private LinkedList<Socket> sockets;
+    private LinkedList<Integer> waiting;
+    private LinkedList<Integer> matched;
+    private int playerNo;
 
-    public PlayerMatch(DataPackage data, MyObjectInputStream objIn, ObjectOutputStream objOut, LinkedList<Socket> sockets) {
+    public PlayerMatch(DataPackage data, MyObjectInputStream objIn, ObjectOutputStream objOut, LinkedList<Integer> waiting, LinkedList<Integer> matched, int playerNo) {
         this.dataPackage = data;
         this.objIn = objIn;
         this.objOut = objOut;
-        this.sockets = sockets;
+        this.waiting = waiting;
+        this.matched = matched;
+        this.playerNo = playerNo;
     }
 
     @Override
     public void run(){
         while(true){
-            if(sockets.size()>=2){
+            if(matched.size()==2 && matched.get(0).equals(playerNo)){
                 DataPackage sendData = new CheckData(2, true);
-
                 try {
                     objOut.writeObject(sendData);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 break;
+            }
+            if(matched.size()==1 && !matched.get(0).equals(playerNo)){
+                matched.add(playerNo);
+                DataPackage sendData = new CheckData(2, true);
+                try {
+                    objOut.writeObject(sendData);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+            if(matched.size()==0){
+                matched.add(playerNo);
             }
         }
     }
